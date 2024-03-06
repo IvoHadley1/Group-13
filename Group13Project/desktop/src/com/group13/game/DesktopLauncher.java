656176486886ecf83a96cc4240ae0a63b72f0524
IvoHadley1 <ih815@york.ac.uk>
2024-03-06 12:32:26 +0000

    Test change

---
+++ Group13Project/desktop/src/com/group13/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.group13.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.group13.game.Group13Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Group-13");
+		new Lwjgl3Application(new Group13Game(), config);
+	}
+}
