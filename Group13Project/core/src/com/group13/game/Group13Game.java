68b91a2e08c7f61e7f4be46df3d154191ff8588c
IvoHadley1 <ih815@york.ac.uk>
2024-03-06 12:25:34 +0000

    Test change

---
+++ Group13Project/core/src/com/group13/game/Group13Game.java
@@ -0,0 +1,33 @@
+package com.group13.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Group13Game extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		int x = 1;
+		//Test change!
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
